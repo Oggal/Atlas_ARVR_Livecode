e37fe866b6efe84c66f5728606ae51e89a18302f
Philip Taylor - Laptop <philip.dan.taylor@gmail.com>
2024-02-19 11:06:42 -0600

    Language Intoduction

---
+++ dotnet/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.ComponentModel;
+using System.Reflection.Metadata.Ecma335;
+using System.Threading;
+
+class Program
+    {
+        static void Main(string[] args)
+        {
+            Counter.Countdown(10);
+
+            Console.WriteLine("Goodbye World!");
+        }
+
+
+
+
+
+    }
+
+class Counter
+{
+    int count;
+    public static void Countdown(int startValue)
+    {
+        for (int index = startValue; index > 0; index--)
+        {
+            Console.WriteLine("{0} is a cool number", index);
+        }
+    } 
+
+    public void Increment(int value = 1)
+    {
+        count += value;
+        Console.WriteLine("Count is now: {0}", count);
+    }
+
+}
+
